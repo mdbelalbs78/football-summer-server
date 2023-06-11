c23537710290ba4413aba22538be6a7e2de251a2
belalhossainS <mdbelal01792@gmail.com>
2023-06-11 13:37:14 +0600

    admin server

--- index.js
+++ index.js
@@ -31,17 +31,34 @@ async function run() {
     const popularCollection = client.db("footballDb").collection("popular");
     const cartCollection = client.db("footballDb").collection("popular");
 
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user);
       const query = {email: user.email}
       const existingUser = await usersCollection.findOne(query)
       console.log('existing user',existingUser);
-      
+
       if(existingUser){
         return res.send({message: 'user already exists'})
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
